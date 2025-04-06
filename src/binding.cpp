8bde6f0878ab7f1a36e8b5e7c97f16c459a06cc2
hop- <hovhannespalyan@gmail.com>
2025-04-07 00:46:39 +0400

    Fix mem issues and restructure the code

--- src/binding.cpp
+++ src/binding.cpp
@@ -1,44 +1,122 @@
 #include <napi.h>
 #include <vips/vips8>
 #include <glib.h>
-#include <iostream>
+#include <string>
+
+std::string detectFormat(const void *buf, const size_t &len)
+{
+  const char *loader = vips_foreign_find_load_buffer(buf, len);
+
+  if (!loader)
+    return "";
+
+  if (strcmp(loader, "VipsForeignLoadJpegBuffer") == 0)
+    return ".jpg";
+  if (strcmp(loader, "VipsForeignLoadPngBuffer") == 0)
+    return ".png";
+  if (strcmp(loader, "VipsForeignLoadWebpBuffer") == 0)
+    return ".webp";
+  if (strcmp(loader, "VipsForeignLoadTiffBuffer") == 0)
+    return ".tif";
+  if (strcmp(loader, "VipsForeignLoadHeifBuffer") == 0)
+    return ".heif";
+  if (strcmp(loader, "VipsForeignLoadJxlBuffer") == 0)
+    return ".jxl";
+  if (strcmp(loader, "VipsForeignLoadGifBuffer") == 0)
+    return ".gif"; // older gif loader
+  if (strcmp(loader, "VipsForeignLoadNsgifBuffer") == 0)
+    return ".gif"; // newer gif loader
+  if (strcmp(loader, "VipsForeignLoadJp2kBuffer") == 0)
+    return ".jp2"; // JPEG2000
+  if (strcmp(loader, "VipsForeignLoadPdfBuffer") == 0)
+    return ".pdf";
+  if (strcmp(loader, "VipsForeignLoadSvgBuffer") == 0)
+    return ".svg";
+  if (strcmp(loader, "VipsForeignLoadRadBuffer") == 0)
+    return ".hdr"; // Radiance
+  if (strcmp(loader, "VipsForeignLoadMagick7Buffer") == 0)
+    return ".magick"; // fallback, could be many formats
+
+  return "";
+}
+
+void resizeImage(
+    const uint8_t *data, const size_t &dataLength,
+    uint8_t **resizedData, size_t *resizedDataLength,
+    const int &width, int height,
+    const char *outFormat = nullptr)
+{
+  const auto image = vips::VImage::new_from_buffer(data, dataLength, "");
+
+  const auto scale = static_cast<double>(width) / image.width();
+  const auto resizedImage = image.resize(scale);
+
+  std::string format;
+  if (outFormat)
+  {
+    format = outFormat;
+  }
+  else
+  {
+    format = detectFormat(data, dataLength);
+    if (format.empty())
+    {
+      throw vips::VError("Unsupported image format");
+    }
+  }
+
+  resizedImage.write_to_buffer(
+      format.c_str(),
+      reinterpret_cast<void **>(resizedData),
+      resizedDataLength);
+}
 
 // Async worker class
-class ResizeWorker : public Napi::AsyncWorker {
+class ResizeWorker : public Napi::AsyncWorker
+{
 private:
-  uint8_t* data;
-  size_t dataLength;
-  uint8_t* resizedData;
+  const uint8_t *data;
+  const size_t dataLength;
+  uint8_t *resizedData;
   size_t resizedDataLength;
-  int width;
-  int height;
+  const int width;
+  const int height;
   Napi::Promise::Deferred promiseDeferred;
 
 public:
-  ResizeWorker(Napi::Env env, Napi::Buffer<uint8_t> buffer, int width, int height, Napi::Promise::Deferred promiseDeferred)
-    : Napi::AsyncWorker(env),
-      data(buffer.Data()),
-      dataLength(buffer.Length()),
-      width(width),
-      height(height),
-      promiseDeferred(promiseDeferred)
-  {}
+  ResizeWorker(Napi::Env &env, Napi::Buffer<uint8_t> &buffer, int &width, int &height, Napi::Promise::Deferred &promiseDeferred)
+      : Napi::AsyncWorker(env),
+        data(buffer.Data()),
+        dataLength(buffer.Length()),
+        resizedData(nullptr),
+        resizedDataLength(0),
+        width(width),
+        height(height),
+        promiseDeferred(promiseDeferred)
+  {
+  }
 
-  ~ResizeWorker() {}
+  ~ResizeWorker()
+  {
+    if (resizedData == nullptr)
+    {
+      g_free(resizedData);
+      resizedData = nullptr;
+      resizedDataLength = 0;
+    }
+  }
 
   void Execute() override
   {
-    try {
-      vips::VImage image = vips::VImage::new_from_buffer(data, dataLength, "");
-      double scale = static_cast<double>(width) / image.width();
-      vips::VImage resizedImage = image.resize(scale);
-
-      resizedImage.write_to_buffer(
-        ".jpeg",
-        reinterpret_cast<void**>(&resizedData),
-        &resizedDataLength
-      );
-    } catch (const vips::VError &e) {
+    try
+    {
+      resizeImage(
+          data, dataLength,
+          &resizedData, &resizedDataLength,
+          width, height);
+    }
+    catch (const vips::VError &e)
+    {
       SetError(e.what());
     }
   }
@@ -49,28 +127,28 @@ private:
     // Napi::Env env = Env();
     Napi::HandleScope scope(Env());
 
-    Napi::Buffer<uint8_t> buffer = Napi::Buffer<uint8_t>::New(
-      Env(),
-      resizedData,
-      resizedDataLength
-    );
+    Napi::Buffer<uint8_t> buffer = Napi::Buffer<uint8_t>::Copy(
+        Env(),
+        resizedData,
+        resizedDataLength);
 
     promiseDeferred.Resolve(buffer);
   }
 
-  void OnError(const Napi::Error& e) override
+  void OnError(const Napi::Error &e) override
   {
     promiseDeferred.Reject(e.Value());
   }
 };
 
-Napi::Value resizeSync(const Napi::CallbackInfo& info)
+Napi::Value resizeSync(const Napi::CallbackInfo &info)
 {
   Napi::Env env = info.Env();
 
-  if (info.Length() < 3 || !info[0].IsBuffer() || !info[1].IsNumber() || !info[2].IsNumber()) {
+  if (info.Length() < 3 || !info[0].IsBuffer() || !info[1].IsNumber() || !info[2].IsNumber())
+  {
     Napi::TypeError::New(env, "Expected (imageBuffer: Buffer, width: number, height: number)")
-      .ThrowAsJavaScriptException();
+        .ThrowAsJavaScriptException();
 
     return env.Null();
   }
@@ -79,75 +157,75 @@ Napi::Value resizeSync(const Napi::CallbackInfo& info)
   int width = info[1].As<Napi::Number>().Int32Value();
   int height = info[2].As<Napi::Number>().Int32Value();
 
-  if (width <= 0 || height <= 0) {
+  if (width <= 0 || height <= 0)
+  {
     Napi::TypeError::New(env, "Width and height must be positive numbers")
-      .ThrowAsJavaScriptException();
-
-    return env.Null();
-  }
-
-  uint8_t* resizedImageBuffer;
-  size_t resizedImageBufferLength = 0;
-
-  try {
-    vips::VImage image = vips::VImage::new_from_buffer(buffer.Data(), buffer.Length(), NULL);
-
-    const double scale = static_cast<double>(width) / image.width();
-
-    image = image.resize(scale);
-
-    image.write_to_buffer(
-      ".jpeg",
-      reinterpret_cast<void**>(&resizedImageBuffer),
-      &resizedImageBufferLength
-    );
-  } catch (const vips::VError &e) {
-    Napi::Error::New(env, e.what()).ThrowAsJavaScriptException();
-
-    return env.Null();
-  }
-
-  return Napi::Buffer<uint8_t>::New(
-    env,
-    resizedImageBuffer,
-    resizedImageBufferLength
-  );
-}
-
-Napi::Value resizeAsync(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
-
-    if (info.Length() < 3 ||
-        !info[0].IsBuffer() ||
-        !info[1].IsNumber() ||
-        !info[2].IsNumber()) {
-        Napi::TypeError::New(env, "Expected (Buffer, width, height)").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-
-    Napi::Buffer<uint8_t> inputBuffer = info[0].As<Napi::Buffer<uint8_t>>();
-
-    int width = info[1].As<Napi::Number>().Int32Value();
-    int height = info[2].As<Napi::Number>().Int32Value();
-    if (width <= 0 || height <= 0) {
-      Napi::TypeError::New(env, "Width and height must be positive numbers")
         .ThrowAsJavaScriptException();
 
-      return env.Null();
-    }
+    return env.Null();
+  }
 
+  uint8_t *resizedImageBuffer = nullptr;
+  size_t resizedImageBufferLength = 0;
+  Napi::Value out;
 
-    Napi::Promise::Deferred deferred = Napi::Promise::Deferred::New(env);
-    ResizeWorker* worker = new ResizeWorker(env, inputBuffer, width, height, deferred);
-    worker->Queue();
+  try
+  {
+    resizeImage(
+        buffer.Data(), buffer.Length(),
+        &resizedImageBuffer, &resizedImageBufferLength,
+        width, height);
 
-    return deferred.Promise();
+    out = Napi::Buffer<uint8_t>::Copy(
+        env,
+        resizedImageBuffer,
+        resizedImageBufferLength);
+  }
+  catch (const vips::VError &e)
+  {
+    Napi::Error::New(env, e.what()).ThrowAsJavaScriptException();
+  }
+
+  g_free(resizedImageBuffer);
+  return out;
 }
 
+Napi::Value resizeAsync(const Napi::CallbackInfo &info)
+{
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 3 ||
+      !info[0].IsBuffer() ||
+      !info[1].IsNumber() ||
+      !info[2].IsNumber())
+  {
+    Napi::TypeError::New(env, "Expected (Buffer, width, height)").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  Napi::Buffer<uint8_t> inputBuffer = info[0].As<Napi::Buffer<uint8_t>>();
+
+  int width = info[1].As<Napi::Number>().Int32Value();
+  int height = info[2].As<Napi::Number>().Int32Value();
+  if (width <= 0 || height <= 0)
+  {
+    Napi::TypeError::New(env, "Width and height must be positive numbers")
+        .ThrowAsJavaScriptException();
+
+    return env.Null();
+  }
+
+  Napi::Promise::Deferred deferred = Napi::Promise::Deferred::New(env);
+  ResizeWorker *worker = new ResizeWorker(env, inputBuffer, width, height, deferred);
+  worker->Queue();
+
+  return deferred.Promise();
+}
 
 Napi::Object init(Napi::Env env, Napi::Object exports)
 {
-  if (VIPS_INIT("cimgres")) {
+  if (VIPS_INIT("cimgres"))
+  {
     Napi::Error::New(env, "Failed to initialize libvips").ThrowAsJavaScriptException();
 
     return exports;
@@ -155,7 +233,6 @@ Napi::Object init(Napi::Env env, Napi::Object exports)
 
   exports.Set(Napi::String::New(env, "resizeSync"), Napi::Function::New(env, resizeSync));
   exports.Set(Napi::String::New(env, "resize"), Napi::Function::New(env, resizeAsync));
-
 
   return exports;
 }
